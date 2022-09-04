0c86b0b7ca6ae608441a7f730dc6c33711f083b5
Ahmed basha <https://github.com/progahmedbasha/Projects.git>
2022-09-04 13:51:02 +0200

    first commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 		<div class="page-title d-flex flex-column align-items-start justify-content-center flex-wrap me-lg-2 pb-5 pb-lg-0" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', lg: '#kt_header_container'}">
 								<!--begin::Heading-->
-								<h1 class="d-flex flex-column text-dark fw-bolder my-0 fs-1">Hello, Paul
+								<h1 class="d-flex flex-column text-dark fw-bolder my-0 fs-1">Hello, {{Auth::user()->name}}
 								<small class="text-muted fs-6 fw-bold ms-1 pt-1">You’ve got 24 New Sales</small></h1>
 								<!--end::Heading-->
 							</div>
