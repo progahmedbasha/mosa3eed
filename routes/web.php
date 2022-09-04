f749ca254aae8e036bca30cf181444adb9c2ef69
Ahmed basha <https://github.com/progahmedbasha/Projects.git>
2022-09-04 12:07:17 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('dashboard', function () {
+    return view('admin.index');
+});
+Route::get('list', function () {
+    return view('admin.list');
+});
+
+Route::group(
+    [
+    
+        'middleware' => [ 'auth:sanctum', 'verified' ],
+    ], function()
+    {
+            Route::get('dashboard', function () {
+                return view('admin.dashboard');
+            });
+     });       
+Route::get('list', function () {
+    return view('admin.list');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
