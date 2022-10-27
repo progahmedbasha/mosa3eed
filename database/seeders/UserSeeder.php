774ff66316d6e8f8a8f0c6e872d5763cd6ca5d6f
Ahmed basha <https://github.com/progahmedbasha/Projects.git>
2022-10-27 16:42:12 +0200

    organization dashboard

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,5 +22,14 @@ class UserSeeder extends Seeder
             'user_type_id' => '1',
             'district_id' => '1',
         ]);
+             DB::table('users')->insert([
+            'name' => 'org',
+            'email' => 'org@gmail.com',
+            'password' => Hash::make('12345678'),
+            'phone' => '123456789',
+            'organization_id' => '1',
+            'user_type_id' => '2',
+            'district_id' => '1',
+        ]);
     }
 }
